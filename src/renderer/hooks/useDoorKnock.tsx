da5b35448828bcd282e0f996d336d46b7bffaee9
Chris McCullough <mcculloughjchris@gmail.com>
2026-08-11 10:39:03 -0500

    random audio knock

--- src/renderer/hooks/useDoorKnock.tsx
+++ src/renderer/hooks/useDoorKnock.tsx
@@ -1,6 +1,15 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import useAudio from "./useAudio"
 
 const useDoorKnock = () => {
+  const { audio, ready } = useAudio()
+  const [ intervalState, setIntervalState ] = useState<NodeJS.Timeout>()
+
+  const handleKnockingAudio = () => {
+    const n = Math.floor(Math.random() * 4)
+    audio.sfx.play(`knock${n}`)
+  }
+
   useEffect(() => {
     const handleDoorKnock = async (data) => {
       const e = new CustomEvent('update-game-data', {
@@ -9,12 +18,17 @@ const useDoorKnock = () => {
         }
       })
       window.dispatchEvent(e)
+
+      const interval = setInterval(handleKnockingAudio, 10000)
+      handleKnockingAudio()
+      setIntervalState(interval)
     }
 
     const unsubscribe = window.electron.ipcRenderer.on('door-knock', handleDoorKnock)
 
     return () => {
       unsubscribe?.()
+      clearInterval(intervalState)
     }
   }, [])
 }
